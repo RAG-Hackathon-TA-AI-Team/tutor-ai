a9ab60985c65a512f7f199f027b55b2fe8d25e8e
Brian Yin <byin@roblox.com>
2024-02-04 06:48:09 -0800

    partial update

--- tutor-ai/backend/app/engine/parser.py
+++ tutor-ai/backend/app/engine/parser.py
@@ -4,7 +4,10 @@ from app.engine.schema import VideoMetaType
 from llama_index.node_parser.interface import NodeParser
 from llama_index.node_parser import SentenceSplitter
 from llama_index.schema import BaseNode, Document
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
 
 SYSTEM_MESSAGE = """\
 You are an expert in describing a sequence of continuous frames in a video. Given
@@ -15,13 +18,29 @@ The resulting text will be used as search index for the video chunk; therefore, 
 concise and descriptive.
 """
 
+
+def get_frames_to_text_chain(encoded_frames: List[str]):
+    vision_llm = ChatOpenAI(
+        model_name="gpt-4-vision-preview"
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content=SYSTEM_MESSAGE),
+            MessagesPlaceholder(variable_name="frames")
+        ]
+    )
+
+    return prompt | vision_llm | StrOutputParser()
+
 def parse_transcript_nodes(docs: List[Document]) -> List[BaseNode]:
     splitter = SentenceSplitter(chunk_size=512, chunk_overlap=32)
     return splitter.get_nodes_from_documents(docs)
 
 
 def parse_keyframe_nodes(docs: List[Document]) -> List[BaseNode]:
-    vision_llm = 
+    frames_to_text_chain = get_frames_to_text_chain([doc.text for doc in docs])
+
 
 
 class VideoNodeParser(NodeParser):
